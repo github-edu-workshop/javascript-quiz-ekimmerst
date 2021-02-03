b1dba50b71a6f14810bacf7daadbe4d68713885b
ekimmerst <e.kimmerstorfer@htl-grieskirchen.at>
2021-02-03 10:00:36 +0100

    homework

--- math.js
+++ math.js
@@ -1,15 +1,15 @@
 exports.add = function (x, y) {
-    // ToDo: Add the necessary code to add two numbers
-    return NaN;
-}
-
-exports.sub = function (x, y) {
-    // ToDo: Add the necessary code to subtract two numbers
-    return NaN;
-}
-
-exports.sumOfPositiveNumbers = function (upperLimitInclusive) {
-    // ToDo: Add the necessary code to calculate the sum of all positive
-    //       numbers lower equal the specified upper limit.
-    return NaN;
-}
+    return x + y;
+  }
+  
+  exports.sub = function (x, y) {
+    return x - y;
+  }
+  
+  exports.sumOfPositiveNumbers = function (upperLimitInclusive) {
+    let result = 0;
+    for (let i = 0; i <= upperLimitInclusive; i++) {
+      result += i;
+    }
+    return result;
+  }
